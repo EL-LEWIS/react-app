e0218fd98738fd74297d1c7c3e0639324d9cb354
Mateus <shingekisnk854@gmail.com>
2024-11-21 19:59:56 -0300

    primeiro commit

---
+++ confusion/README.md
@@ -0,0 +1,16 @@
+## Resumo da Configuração
+
+---
+1. instalando as dependencia reactstrap, bootstrap e react-popper
+configurando o bootstrap no arquivo index.js (import 'bootstrap/dist/css/bootstrap.min.css';
+) 
+
+---
+
+2. Alteraçao da barra de navegaçao utilizando reactstrap no arquivo app.js e alterando o div para Mateus Felipe
+
+
+---
+3. instalando e configurando o servidor do reactapp e em seguida npm start!
+
+![alt text](<../Captura de tela 2024-11-21 194227.png>)
---
+++ confusion/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <div>Aluno: [Mateus Felipe]</div>
+          </div>
+        </Navbar>
+    </div>
+  );
+}
+
+export default App;
